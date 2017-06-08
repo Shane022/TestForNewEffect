0f1fc5429caf88735aa5cc352022da3f5fb4d7bd
shangzhixin <shangzhixin@sumaplayer.com>
2017-06-08 09:19:49 +0800

    first commit

---
+++ TestForNewEffect/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestForNewEffect
+//
+//  Created by dvt04 on 17/5/3.
+//  Copyright © 2017年 suma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestForNewEffect/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  TestForNewEffect
+//
+//  Created by dvt04 on 17/5/3.
+//  Copyright © 2017年 suma. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
